f7f7f4f1e2da41408055f438c55792f7116881d5
paulobr4z <paulobrazaraujo@gmail.com>
2021-11-15 12:26:17 -0300

    2-Componentização

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logoImg from '../../assets/logo.svg'
+import {
+  Container,
+  Content
+} from './styles'
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
